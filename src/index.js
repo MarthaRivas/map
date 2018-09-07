0873f902f63f3eb47322ee038ae6edb66c46d795
MarthaRivas <groenezeeriv@gmail.com>
2018-09-07 11:01:56 +0200

    table top gecreernd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 var crel = require ('crel')
+var tabletop = require ('tabletop')
 
-document.body.appendChild(crel ('h1','hello world'))
+tabletop.init({
+    key: 'https://docs.google.com/spreadsheets/d/14GTxRdrZgGe2sLrxX-KErQnWuNR2St25qgJHZXLNrhk/pubhtml',
+    callback: function (data, tabletop) {
+      console.log(data)
+    },
+    simpleSheet: true
+   })
+
+
+
+   console.log('data')
